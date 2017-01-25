111117dba203b6af230157ff300f063ad16288fe
eaglerayp <eagle_rayp@hotmail.com>
2017-01-25 09:38:02 +0800

    add some info and error handling

--- termTitle.go
+++ termTitle.go
@@ -9,8 +9,17 @@ import (
 
 const prefix = "\\033]0;"
 const postfix = "\\007"
+const version = "v0.1.0"
+
+func helpMsg() {
+	fmt.Printf("TermTitle\nVersion:%s\nChange the terminal title in toolbar.\n\nUsage:\n    TermTitle [title]\n", version)
+}
 
 func main() {
+	if len(os.Args) < 2 {
+		helpMsg()
+		return
+	}
 	title := os.Args[1]
 	arg := prefix + title + postfix
 	cmd := exec.Command("echo", "-ne", arg)
